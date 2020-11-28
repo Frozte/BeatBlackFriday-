251c9c3af68bb5f8887c902f7f44914530871e87
Frozte <deeppatel@live.com>
2020-11-28 01:53:04 -0500

    Heroku Deployment Test

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
 from flask import Flask, request, render_template, jsonify
 import requests 
 import json
+import os
+<<<<<<< Updated upstream
+
+=======
+>>>>>>> Stashed changes
 from selenium import webdriver
+
 from selenium.webdriver.common.keys import Keys
-#from selenium.webdriver.chrome.options import Options
 from product import Product
 from utils import convert_price_toNumber
-
-#from price_scraper import FUNCTION NAME
+from web_driver_conf import get_web_driver_options
+from web_driver_conf import get_chrome_web_driver
+from web_driver_conf import set_ignore_certificate_error
+from web_driver_conf import set_browser_as_incognito
+from web_driver_conf import set_automation_as_head_less
 
 app = Flask(__name__)
 
@@ -20,38 +28,50 @@ def index():
 @app.route('/get-text', methods=['GET', 'POST'])
 def foo():
 
-    # GOOGLE_CHROME_BIN = '/app/.apt/usr/bin/google_chrome'
-    # CHROMEDRIVER_PATH = '/app/.chromedriver/bin/chromedriver'
-
     bar = request.form['test']
     
     URL = "http://www.amazon.com/"
-    NUMBER_OF_PAGES_TO_SEARCH = 1
+    NUMBER_OF_PAGES_TO_SEARCH = 5
     QUESTION_PRODUCT = "What are you looking for?\n:"
-    PRODUCT_PATH = '//*[@id="search"]/div[1]/div[2]/div/span[3]/div[2]/div'
-    search_term = str(bar)
+    search_term = str(bar) #PASS USER INPUT FROM HTML TO HERE
 
     biggest_discount = 0.0
     lowest_price = 0.0
-    chepest_product = Product("", "", "", "", "", "")
-    best_deal_product = Product("", "", "", "", "", "")
+    chepest_product = Product("", "", "", "")
+    best_deal_product = Product("", "", "", "")
     search_terms = search_term.split(" ")
+
+<<<<<<< Updated upstream
+    options = webdriver.ChromeOptions()
+    options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    options.add_argument("--headless")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--no-sandbox")
+    options.add_argument('--ignore-certificate-errors')
+    options.add_argument('--incognito')
+    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=options)
+=======
+    # GOOGLE_CHROME_BIN = '/app/.apt/usr/bin/google_chrome' #PATH SET ON HEROKU CONFIG_VARS
+    # CHROMEDRIVER_PATH = '/app/.chromedriver/bin/chromedriver' #PATH SET ON HEROKU CONFIG_VARS
 
     #####################################
     options = webdriver.ChromeOptions()
-    options.add_argument('--headless')
-    options.add_argument('--ignore-certificate-errors')
-    options.add_argument('--incognito')
-    driver = webdriver.Chrome("chromedriver.exe", options=options)
+    options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
     #####################################
+    options.add_argument('--headless')
+    options.add_argument("--disable-gpu")
+    options.add_argument("--no-sandbox")
+    #####################################
+    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=options)
+    #driver = webdriver.Chrome("chromedriver.exe", options=options) ##UNCOMMENT FOR TESTING
+    #####################################
+>>>>>>> Stashed changes
 
     driver.get(URL)
     element = driver.find_element_by_xpath('//*[@id="twotabsearchtextbox"]')
     element.send_keys(search_term)
     element.send_keys(Keys.ENTER)
-
     products = []
-
     page = NUMBER_OF_PAGES_TO_SEARCH
 
     while True:
@@ -60,54 +80,43 @@ def foo():
                 driver.get(driver.current_url + "&page=" + str(page))
             except:
                 break
-        
-        for i in driver.find_elements_by_xpath(PRODUCT_PATH):
-            should_add = True
-            name = ""
-            price = ""
-            prev_price = ""
-            link = ""
-            discount = 0.0
-            # rating = ""
-            prime = False
-            try:
-                h2tag = i.find_element_by_tag_name('h2')
-                name = h2tag.text
-                price = convert_price_toNumber(i.find_element_by_class_name('a-price').text)
-                link = h2tag.find_element_by_tag_name('a').get_attribute("href")
-                # rating = 
+
+        for i in driver.find_elements_by_xpath('//*[@id="search"]/div[1]/div[2]/div/span[3]/div[2]'):
+            counter = 0
+            for element in i.find_elements_by_xpath('//div/span/div/div/div/div'):
+                should_add = True
+                name = ""
+                price = ""
+                prev_price = ""
+                link = ""
                 try:
-                    prime_element = i.find_element_by_class_name("a-icon-prime")
-                    print(prime_element)
-                    prime = True
+                    name = i.find_elements_by_tag_name('h2')[counter].text
+                    price = convert_price_toNumber(element.find_element_by_class_name('a-price').text)
+                    link = i.find_elements_by_xpath('//h2/a')[counter].get_attribute("href")
+                    try:
+                        prev_price = convert_price_toNumber(element.find_element_by_class_name('a-text-price').text)
+                    except:
+                        Exception()
+                        prev_price = price
                 except:
-                    Exception()
-                try:
-                    prev_price = convert_price_toNumber(i.find_element_by_class_name('a-text-price').text)
-                    discount = (prev_price-price)/prev_price*100
-                except:
-                    Exception()
-                    prev_price = price
-            except:
-                # print("exception")
-                should_add = False
-            
-            product = Product(name, price, prev_price, discount, link, prime)
-            if should_add:
-                products.append(product)
-                # print(products)
-                
+                    print("exception")
+                    should_add = False
+                product = Product(name, price, prev_price, link)
+                if should_add:
+                    products.append(product)
+                    print(products)
+                counter = counter + 1
         page = page - 1
         if page == 0:
             break
         print(page)
-
     run = 0
+
     for product in products:
         not_right = False
-        # for word in search_terms:
-        #     if word.lower() not in product.name.lower():
-        #         not_right = True
+        for word in search_terms:
+            if word.lower() not in product.name.lower():
+                not_right = True
         if not not_right:
             if run == 0:
                 lowest_price = product.price
@@ -116,9 +125,9 @@ def foo():
             elif product.price < lowest_price:
                 lowest_price = product.price
                 chepest_product = product
-            if product.discount > biggest_discount:
-                biggest_discount = product.discount
-                print(product.discount)
+            discount = product.prev_price - product.price
+            if discount > biggest_discount:
+                biggest_discount = discount
                 best_deal_product = product
 
     with open('products.json', 'w') as json_file:
@@ -131,11 +140,22 @@ def foo():
     print(json.dumps(chepest_product.serialize(), indent=4, sort_keys=True))
     print(json.dumps(best_deal_product.serialize(), indent=4, sort_keys=True))
 
-    ##############################
     options = webdriver.ChromeOptions()
+    options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+<<<<<<< Updated upstream
     options.add_argument('--ignore-certificate-errors')
-    driver = webdriver.Chrome("chromedriver.exe", options=options)
+    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=options)
+=======
     ##############################
+    #options.add_argument('--headless')
+    #options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--no-sandbox")
+    ##############################
+    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=options)
+    #driver = webdriver.Chrome("chromedriver.exe", options=options)
+    ##############################
+>>>>>>> Stashed changes
 
     driver.get(best_deal_product.link)
     driver.find_element_by_tag_name('body').send_keys(Keys.COMMAND + 't')
