e87088624daae709c86128fd09d71d72cf4cf709
Frozte <deeppatel@live.com>
2020-11-28 02:12:38 -0500

    Update app.py

--- app.py
+++ app.py
@@ -21,14 +21,15 @@ def foo():
     bar = request.form['test']
     
     URL = "http://www.amazon.com/"
-    NUMBER_OF_PAGES_TO_SEARCH = 5
+    NUMBER_OF_PAGES_TO_SEARCH = 1
     QUESTION_PRODUCT = "What are you looking for?\n:"
-    search_term = str(bar) #PASS USER INPUT FROM HTML TO HERE
+    PRODUCT_PATH = '//*[@id="search"]/div[1]/div[2]/div/span[3]/div[2]/div'
+    search_term = str(bar)
 
     biggest_discount = 0.0
     lowest_price = 0.0
-    chepest_product = Product("", "", "", "")
-    best_deal_product = Product("", "", "", "")
+    chepest_product = Product("", "", "", "", "", "")
+    best_deal_product = Product("", "", "", "", "", "")
     search_terms = search_term.split(" ")
 
     # GOOGLE_CHROME_BIN = '/app/.apt/usr/bin/google_chrome' #PATH SET ON HEROKU CONFIG_VARS
@@ -50,7 +51,9 @@ def foo():
     element = driver.find_element_by_xpath('//*[@id="twotabsearchtextbox"]')
     element.send_keys(search_term)
     element.send_keys(Keys.ENTER)
+
     products = []
+
     page = NUMBER_OF_PAGES_TO_SEARCH
 
     while True:
@@ -59,43 +62,54 @@ def foo():
                 driver.get(driver.current_url + "&page=" + str(page))
             except:
                 break
-
-        for i in driver.find_elements_by_xpath('//*[@id="search"]/div[1]/div[2]/div/span[3]/div[2]'):
-            counter = 0
-            for element in i.find_elements_by_xpath('//div/span/div/div/div/div'):
-                should_add = True
-                name = ""
-                price = ""
-                prev_price = ""
-                link = ""
+        
+        for i in driver.find_elements_by_xpath(PRODUCT_PATH):
+            should_add = True
+            name = ""
+            price = ""
+            prev_price = ""
+            link = ""
+            discount = 0.0
+            # rating = ""
+            prime = False
+            try:
+                h2tag = i.find_element_by_tag_name('h2')
+                name = h2tag.text
+                price = convert_price_toNumber(i.find_element_by_class_name('a-price').text)
+                link = h2tag.find_element_by_tag_name('a').get_attribute("href")
+                # rating = 
                 try:
-                    name = i.find_elements_by_tag_name('h2')[counter].text
-                    price = convert_price_toNumber(element.find_element_by_class_name('a-price').text)
-                    link = i.find_elements_by_xpath('//h2/a')[counter].get_attribute("href")
-                    try:
-                        prev_price = convert_price_toNumber(element.find_element_by_class_name('a-text-price').text)
-                    except:
-                        Exception()
-                        prev_price = price
+                    prime_element = i.find_element_by_class_name("a-icon-prime")
+                    print(prime_element)
+                    prime = True
                 except:
-                    print("exception")
-                    should_add = False
-                product = Product(name, price, prev_price, link)
-                if should_add:
-                    products.append(product)
-                    print(products)
-                counter = counter + 1
+                    Exception()
+                try:
+                    prev_price = convert_price_toNumber(i.find_element_by_class_name('a-text-price').text)
+                    discount = (prev_price-price)/prev_price*100
+                except:
+                    Exception()
+                    prev_price = price
+            except:
+                # print("exception")
+                should_add = False
+            
+            product = Product(name, price, prev_price, discount, link, prime)
+            if should_add:
+                products.append(product)
+                # print(products)
+                
         page = page - 1
         if page == 0:
             break
         print(page)
-    run = 0
 
+    run = 0
     for product in products:
         not_right = False
-        for word in search_terms:
-            if word.lower() not in product.name.lower():
-                not_right = True
+        # for word in search_terms:
+        #     if word.lower() not in product.name.lower():
+        #         not_right = True
         if not not_right:
             if run == 0:
                 lowest_price = product.price
@@ -104,9 +118,9 @@ def foo():
             elif product.price < lowest_price:
                 lowest_price = product.price
                 chepest_product = product
-            discount = product.prev_price - product.price
-            if discount > biggest_discount:
-                biggest_discount = discount
+            if product.discount > biggest_discount:
+                biggest_discount = product.discount
+                print(product.discount)
                 best_deal_product = product
 
     with open('products.json', 'w') as json_file:
@@ -118,6 +132,7 @@ def foo():
 
     print(json.dumps(chepest_product.serialize(), indent=4, sort_keys=True))
     print(json.dumps(best_deal_product.serialize(), indent=4, sort_keys=True))
+
 
     options = webdriver.ChromeOptions()
     options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
